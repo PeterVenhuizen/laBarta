14cb5b7f9bbd5d9dca675435dc152d32ebc09977
PeterVenhuizen <peter_venhuizen@hotmail.com>
2017-02-21 10:53:19 +0100

    Added fastQ iterator

--- fileParser.py
+++ fileParser.py
@@ -83,3 +83,50 @@ def yield_fasta(f):
 			identifier = line[1:].rstrip()
 		else: seq += line.rstrip()
 	yield(SeqRecord(identifier, seq))
+
+class parseFastQ(object):
+	""" Returns read-by-read fastQ records as tuple: (sequence identifier, raw sequence, quality identifier, quality sequence). """
+
+	def __init__(self, fastq_file):
+		if fastq_file.endswith('gz'): self.fastq_file = gzip.open(fastq_file)
+		else: self.fastq_file = open(fastq_file)
+		self.lineNumber = 0
+	
+	def __iter__(self):
+		return self
+		
+	def next(self):
+		
+		# A fastQ record should consist of 4 lines
+		record = []
+		for i in xrange(4):
+			line = self.fastq_file.readline()
+			self.lineNumber += 1
+			
+			if line: record.append(line.rstrip())
+			else: record.append(None)
+			
+		# Inspect the record
+		nValues = [ bool(x) for x in record ].count(True)
+		
+		# End of file
+		if record.count(None) == 4:
+			raise StopIteration
+		
+		# Check for premature EOF or empty line
+		assert nValues == 4, \
+			"ERROR: Premature EOF or empty line found near line number %d" % (self.lineNumber)
+		
+		# Check for "@" header
+		assert record[0].startswith('@'), \
+			"ERROR: The 1st line of the record does not start with '@'. Please check near %d" % (self.lineNumber-3)
+
+		# Check for "+" header
+		assert record[2].startswith('+'), \
+			"Error: The 3rd line of the record does not start with '+'. Please check near %d" % (self.lineNumber-1)
+			
+		# Compare sequence and quality length
+		#assert len(record[1]) == len(record[3]), \
+		#	"Error: The length of the sequence data and the quality data are not equal. Please check near %d" % (self.lineNumber-2)
+			
+		return tuple(record)
