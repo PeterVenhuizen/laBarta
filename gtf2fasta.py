512db0ab366e83f1dee57db85f99f448f733d562
PeterVenhuizen <peter_venhuizen@hotmail.com>
2017-02-21 15:24:21 +0100

    Integrated parse_GTF from fileParser.py

--- gtf2fasta.py
+++ gtf2fasta.py
@@ -5,25 +5,18 @@ Create a spliced Salmon/KALLISTO transcriptome fasta file from a GTF file.
 """
 
 from natsort import natsorted
-from fileParser import yield_fasta
+from fileParser import yield_fasta, parse_GTF
 import argparse
 import subprocess
 
-def gtf2fasta(gtf, genome_fasta):
+def gtf2fasta(gtf_file, genome_fasta, select_feature="exon", t_id_attr="transcript_id", attr_sep=' "'):
 
 	# Parse gtf and create exon bed
+	gtf = parse_GTF(gtf_file, select_feature, t_id_attr, attr_sep, False)
 	with open('tmp.bed', 'w') as fout:
-		for line in gtf:
-			if not line.startswith('#'):
-				c, source, feature, s, e, score, strand, phase, attributes = line.rstrip().split('\t')
-				attr = {}
-				for a in attributes.split(';'):
-					if len(a):
-						attr_name, attr_value = a.split(' "')
-						attr[attr_name.strip()] = attr_value.replace('\"', '')
-
-				if feature == "exon":
-					fout.write( '{0}\t{1}\t{2}\t{3}_{0}:{1}-{2}_{4}\t1000\t{4}\n'.format(c, int(s)-1, e, attr['transcript_id'], strand) )
+		for t_id in natsorted(gtf):
+			for s, e in gtf[t_id]['exons']:
+				fout.write( '{0}\t{1}\t{2}\t{3}_{0}:{1}-{2}_{4}\t1000\t{4}\n'.format(gtf[t_id]['chr'], s-1, e, t_id, gtf[t_id]['strand']) )
 
 	# Get exon fasta
 	cmd = "bedtools getfasta -name -s -fi {} -bed tmp.bed -fo tmp.fa".format(genome_fasta)
@@ -56,8 +49,11 @@ def gtf2fasta(gtf, genome_fasta):
 if __name__ == '__main__':
 	
 	parser = argparse.ArgumentParser(description=__doc__)
-	parser.add_argument('-g', '--gtf', type=argparse.FileType('r'), default='-', help="Input GTF file")
+	parser.add_argument('-g', '--gtf', required=True, help="Input GTF file")
 	parser.add_argument('-f', '--genome-fasta', required=True, help="Genomic sequence fasta")
+	parser.add_argument('--select-feature', default="exon", help="Feature to select in the GTF file")
+	parser.add_argument('--transcript-attr', default='transcript_id', help="The transcript identifier attribute name")
+	parser.add_argument('--attr-sep', default=' "', help="GFF attribute and attribute value separator")
 	args = parser.parse_args()
 
-	gtf2fasta(args.gtf, args.genome_fasta)
+	gtf2fasta(args.gtf, args.genome_fasta, args.select_feature, args.transcript_attr, args.attr_sep)
