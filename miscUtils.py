d0aba898d2277821d77326545ac54ee87c02135d
PeterVenhuizen <peter_venhuizen@hotmail.com>
2017-02-21 15:07:32 +0100

    Initialize miscUtils.py

---
+++ miscUtils.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+
+"""
+A collection of miscellaneous useful functions.
+"""
+
+def roundrobin(*iterables):
+    "roundrobin('ABC', 'D', 'EF') --> A D E B F C"
+    # Recipe credited to George Sakkis
+    pending = len(iterables)
+    nexts = cycle(iter(it).next for it in iterables)
+    while pending:
+        try:
+            for next in nexts:
+                yield next()
+        except StopIteration:
+            pending -= 1
+            nexts = cycle(islice(nexts, pending))
