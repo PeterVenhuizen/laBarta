d7e9db434ab06e4ffe1a4d0251ddfcd82b7d2964
PeterVenhuizen <peter_venhuizen@hotmail.com>
2017-03-14 11:03:37 +0100

    Add switched isoforms to stdout and changed plotting to mean + stdev

--- find_isoform_switches.py
+++ find_isoform_switches.py
@@ -11,34 +11,50 @@ To-do:
 """
 
 from __future__ import division
+from collections import Counter
 from natsort import natsorted
 from fileParser import parse_salmon
+import numpy as np
 import subprocess
 import argparse
 import os
 import operator
 
-def is_switched(trs1, trs2):
-	
-	order1 = sorted(trs1.iteritems(), key=operator.itemgetter(1), reverse=True)
-	order2 = sorted(trs2.iteritems(), key=operator.itemgetter(1), reverse=True)
+def all_switched(g_id, s1, s2):
 
-	names1 = [ o[0] for o in order1 ]
-	names2 = [ o[0] for o in order2 ]
+	switches = Counter()
+	N = len(s1)
 
-	for i, x1 in enumerate(names1):
-		x2 = names2[i]
-		if x1 != x2:
-			one = "before" if names1.index(x1) < names1.index(x2) else "after"
-			two = "before" if names2.index(x1) < names2.index(x2) else "after"
+	for i in xrange(N):
 
-			if one != two:
-				if all([ trs1[x1] >= 1, trs1[x2] >= 1, trs2[x1] >= 1, trs2[x2] >= 1 ]):
-					return True
+		order1 = sorted(s1[i][g_id].iteritems(), key=operator.itemgetter(1), reverse=True)
+		order2 = sorted(s2[i][g_id].iteritems(), key=operator.itemgetter(1), reverse=True)
 
-	return False
+		names1 = [ o[0] for o in order1 ]
+		names2 = [ o[0] for o in order2 ]
 
-def plotify(s1_files, s2_files, selection_file, output_dir):
+		for j, x1 in enumerate(names1):
+			x2 = names2[j]
+			if x1 != x2:
+				one = "before" if names1.index(x1) < names1.index(x2) else "after"
+				two = "before" if names2.index(x1) < names2.index(x2) else "after"
+
+				if one != two and all([ s1[i][g_id][x1] >= 1, s1[i][g_id] >= 1, s2[i][g_id] >= 1, s2[i][g_id] >= 1 ]):
+					switches[x1+'-'+x2] += 1
+
+	is_switched = False
+	printed = set()
+	for x in switches:
+		if switches[x] == N:
+			x1, x2 = natsorted(x.split('-'))
+			if (x1, x2) not in printed:
+				printed.add((x1, x2))
+				print '{}\t{}\t{}'.format(g_id, x1, x2)
+				is_switched = True
+
+	return is_switched
+
+def find_isoform_switches(s1_files, s2_files, selection_file, output_dir):
 	
 	s1 = [ parse_salmon(f) for f in s1_files ]
 	s2 = [ parse_salmon(f) for f in s2_files ]
@@ -49,35 +65,36 @@ def plotify(s1_files, s2_files, selection_file, output_dir):
 	for g_id in natsorted(genes):
 		t_ids = natsorted(s1[0][g_id].keys())
 
-		if len(t_ids) > 1 and all([ is_switched(s1[i][g_id], s2[i][g_id]) for i in xrange(0, len(s1)) ]):
-			print g_id
+		#if len(t_ids) > 1 and all([ is_switched(s1[i][g_id], s2[i][g_id]) for i in xrange(0, len(s1)) ]):
+		if len(t_ids) > 1 and all_switched(g_id, s1, s2):
 
 			with open('save_dot_plot.R', 'w') as fout:
 				fout.write( "#!/usr/bin/env Rscript\n" )
 				fout.write( "library(ggplot2)\n" )
 
 				fout.write( "df <- data.frame(\n" )
-				fout.write( "\tisoform = rep(c( {} ), each={}),\n".format( ', '.join(['"'+t+'"' for t in t_ids]), len(s1)+len(s2)) )
-				fout.write( "\tcondition = rep(c( \"ST\", \"7-1-1\" ), each={}),\n".format( len(s1) ) )
-				
-				fout.write( "\tfullname = c(\n" )
-				fout.write(  ',\n'.join([ "\t\trep(c({}), 2)".format( ', '.join([ '"'+t+"_"+str(r+1)+'"' for r in xrange(len(s1)) ]) ) for t in t_ids ]) )
+				fout.write( "\tisoform = rep(c( {} ), each=2),\n".format( ', '.join(['"'+t+'"' for t in t_ids])) )
+				fout.write( "\tcondition = rep(c( \"ST\", \"7-1-1\" ), {}),\n".format( len(t_ids) ) )
+
+				fout.write( "\tTPM_avg = c(\n" )
+				fout.write( ',\n'.join([ "\t\t{}, {}".format( np.mean([ s[g_id][t] for s in s1 ]), np.mean([ s[g_id][t] for s in s2 ]) ) for t in t_ids ]) )
 				fout.write( "\n\t),\n" )
 
-				fout.write( "\tPWM = c(\n" )
-				fout.write( ',\n'.join([ "\t\t{}".format( ', '.join([ str(s[g_id][t]) for s in s1+s2 ]) ) for t in t_ids ]))
-				fout.write( "\n\t)\n)" )
+				fout.write( "\tstdev = c(\n" )
+				fout.write( ',\n'.join([ "\t\t{}, {}".format( np.std([ s[g_id][t] for s in s1 ]), np.std([ s[g_id][t] for s in s2 ]) ) for t in t_ids ]) )
+				fout.write( "\n\t)\n)")
 
 				fout.write( "\ndf$condition <- factor(df$condition, levels=c(\"ST\", \"7-1-1\"))" )
+				fout.write( "\nlimits <- aes(ymax = TPM_avg + stdev, ymin = TPM_avg - stdev, colour=isoform)")
 				fout.write( "\npng(file=\"{}/{}.png\", width=450, height=600)".format(output_dir, g_id) )
-				fout.write( "\nggplot(data=df, aes(x=condition, y=PWM, group=fullname)) +" )
+				fout.write( "\nggplot(data=df, aes(x=condition, y=TPM_avg, group=isoform)) +" )
 				fout.write( "\n\tggtitle(\"{}\") + theme(plot.title = element_text(hjust = 0.5)) +".format(g_id) )
 				fout.write( "\n\tgeom_point(aes(colour=isoform), size=3) +" )
+				fout.write( "\n\tgeom_errorbar(limits, width=0.05, size=0.35) +" )
 				fout.write( "\n\tgeom_line(size=1, alpha=0.5, aes(colour=isoform)) +" )
-				fout.write( "\n\tlabs(x=\"\", y=\"PWM\", fill=\"\")")
+				fout.write( "\n\tlabs(x=\"\", y=\"TPM mean\", fill=\"\")")
 
 			subprocess.call("Rscript save_dot_plot.R", shell=True)
-
 
 if __name__ == '__main__':
 
@@ -89,4 +106,4 @@ if __name__ == '__main__':
 	parser.add_argument('--selection', help="Gene selection file. Gene identifier should be in the first column.")
 	args = parser.parse_args()
 
-	plotify(args.s1, args.s2, args.selection, args.output_dir)
+	find_isoform_switches(args.s1, args.s2, args.selection, args.output_dir)
