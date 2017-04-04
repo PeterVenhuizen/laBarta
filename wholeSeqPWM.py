846d90e513ccd1266d737cb00e5c481fbcfb03bf
PeterVenhuizen <peter_venhuizen@hotmail.com>
2017-04-04 15:49:52 +0200

    Update wholeSeqPWM.py

--- wholeSeqPWM.py
+++ wholeSeqPWM.py
@@ -33,10 +33,45 @@ def read_PWM_from_file(f):
 	
 	return pwm
 
+def score_seq(seq, PWM):
+	return sum([ log((PWM[i][base]/0.25)+0.0001, 2) for i, base in enumerate(seq) if base in PWM[i].keys() ])
+
+def wholeSeqPWM(background_fasta, soi_fasta, PWM_files, min_score=65):
+	
+	# Parse PWM files
+	for f in PWM_files:
+		PWM = { 'pwm': read_PWM_from_file(f), 'min': 0, 'max': 0 }
+		k = len(PWM['pwm']) # Get PWM size
+		
+		kmers = {}
+		# Iterate over sequences and score the kmers in the same
+		# size as the PWMs.
+		for record in yield_fasta(background_fasta):
+			
+			N = (len(record.seq)-k)+1
+			for i in xrange(0, N, 1):
+				
+				kmer = record.seq[i:i+k]
+				
+				try:
+					kmers[kmer]['IDs'].add( record.id )
+				except KeyError: 
+
+					# Score and store
+					score = score_seq(kmer, PWM['pwm'])
+					kmers[kmer] = { 'score': score, 'IDs': set([ record.id ]) }
+
+					# Update min and max scores
+					PWM['min'] = score if score < PWM['min'] else pwm['min']
+					PWM['max'] = score if score > PWM['max'] else pwm['max']
+	
+	# Rescale the scores and output hits above the min_score cutoff
+
 if __name__ == '__main__':
 	
 	parser = argparse.ArgumentParser(description=__doc__)
-	parser.add_argument('-f', '--fasta', required=True, help="Fasta input file")
+	parser.add_argument('--soi', required=True, help="Fasta file containing the sequences of interest.")
+	parser.add_argument('-b', '--background', required=True, help="Fasta file with 'background' sequence to compare against.")
 	parser.add_argument('--PWM', required=True, nargs='+', help="PWM files (output from get_PWMs.py) or similarly structured files.")
 	parser.add_argument('--min-score', type=int, default=65, help="Minimal PWM score cutoff for sequence reporting")
 	args = parser.parse_args()
