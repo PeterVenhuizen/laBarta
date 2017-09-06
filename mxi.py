40d4f7ea571bcf7a6f9800b6bc4f3f2e4af83b48
PeterVenhuizen <peter_venhuizen@hotmail.com>
2017-09-06 12:07:53 +0200

    Updated MXI detection to include overlapping IRs with multiple retained introns.

--- mxi.py
+++ mxi.py
@@ -4,9 +4,7 @@
 mxi.py => Detect Mutually Exclusive Introns in a given GTF file. 
 
 	---|||||-----|||||||||||||||---
-
 	---|||||||||||||||-----|||||---
-
 	---|||||-----|||||-----|||||---
 
 	AStalavista MXI code => 1^2-,3^4-
@@ -56,14 +54,14 @@ def mxi(gtf_file):
 			transcripts = { t_id: gtf[t_id] for t_id in groups[g_id] }
 			first_id = transcripts.keys()[0]
 			chromosome, strand = [ transcripts[first_id][x] for x in ['chr', 'strand'] ]
-			ir_events = {}
+			ir_exons = {}
 
+			# Get IR exons
 			for t_id in natsorted(transcripts):
 				for s, e in transcripts[t_id]['exons']:
 
-					event_id = '{}-{}'.format(s, e)
-
 					# Look for overlapping exons in other isoforms
+					exon_id = '{}-{}'.format(s, e)
 					for other_t in transcripts:
 						if t_id != other_t:
 
@@ -72,45 +70,49 @@ def mxi(gtf_file):
 							if len(matches) > 1:
 
 								# Check whether the overlapping exons are actually not
-								# in fact alternative 3' and 5' spliced exons
+								# in fact alternative 5' and 3' spliced exons
 								a, b = matches[0]
 								if any([ not is_overlapping(a, b, c, d) for c, d in matches[1:] ]):
+									try: ir_exons[exon_id]['trs'].add(t_id)
+									except KeyError: ir_exons[exon_id] = { 'trs': set([ t_id ]), 'introns': set() }
+			ir_list = [ map(int, ir.split('-')) for ir in ir_exons ]
 
-									try: ir_events[event_id].add(t_id)
-									except KeyError: ir_events[event_id] = set([ t_id ])
+			# Assign the retained introns
+			introns = {}
+			for t_id in natsorted(transcripts):
+				for s, e in transcripts[t_id]['introns']:
 
-			# Retrieve the retained introns
-			all_introns = set([ (x, y) for x, y in list(itertools.chain(*[ transcripts[t_id]['introns'] for t_id in transcripts ])) ])
-			all_IR = [ map(int, ir.split('-')) for ir in ir_events ]
-			ret_introns = {}
-			for s, e in all_IR:
-				introns = [ [x, y] for x, y in all_introns if is_contained_in(x, y, s, e) ]
-				if len(introns) == 1: ret_introns['{}-{}'.format(s, e)] = introns[0]
+					# Save intron/transcript info
+					intron_id = '{}-{}'.format(s, e)
+					try: introns[intron_id].add(t_id)
+					except KeyError: introns[intron_id] = set([ t_id ])
 
-			# Get the alternative transcripts
-			alt_trs = {}
-			for i in ret_introns:
-				ir = '{}-{}'.format(*ret_introns[i])
-				try: alt_trs[ir] = alt_trs[ir] | ir_events[i]
-				except KeyError: alt_trs[ir] = ir_events[i]
+					for x, y in ir_list:
+						if is_contained_in(s, e, x, y): ir_exons['{}-{}'.format(x, y)]['introns'].add((s, e))
 
 			# Look for overlapping IR events
-			all_IR = [ map(int, ir.split('-')) for ir in ret_introns ]
-			while len(all_IR) > 1:
-				s, e = all_IR.pop()
-				matches = [ [x, y] for x, y in all_IR if is_overlapping(s, e, x, y) ]
+			while len(ir_list) >= 1:
+				s, e = ir_list.pop()
+				matches = [ [x, y] for x, y in ir_list if is_overlapping(s, e, x, y) ]
+
 				if len(matches):
 
-					# Check that the retained introns are not the same and 
-					# that they share a common "middle" exon
-					ir1, ir2 = [ '{}-{}'.format(x, y) for x, y in natsorted([[s, e], [matches[0][0], matches[0][1]]]) ]
-					if ret_introns[ir1] != ret_introns[ir2] and ret_introns[ir1][1]+1 == int(ir2.split('-')[0]):
+					ir1, ir2 = [ '{}-{}'.format(x, y) for x, y in natsorted([ [s, e], [matches[0][0], matches[0][1]] ]) ]
 
-						try: 
-							ir1_alt = '{}-{}'.format(*ret_introns[ir1])
-							ir2_alt = '{}-{}'.format(*ret_introns[ir2])
-							print '{0}\t{1}\t{1};MXI:{0}:{2}:{3}-{4}:{5}-{6}:{7}:{8}\t{9}\t{10}'.format(chromosome, g_id, ir1.split('-')[0], ret_introns[ir1][0], ret_introns[ir1][1], ret_introns[ir2][0], ret_introns[ir2][1], ir2.split('-')[1], strand, ','.join(alt_trs[ir1_alt]), ','.join(alt_trs[ir1_alt]|alt_trs[ir2_alt]))
-						except IndexError, e: print e
+					# Transform sets to lists
+					ir_exons[ir1]['introns'] = list(ir_exons[ir1]['introns'])
+					ir_exons[ir2]['introns'] = list(ir_exons[ir2]['introns'])
+
+					# Iterate over the potentially multiple retained introns
+					for i in ir_exons[ir1]['introns']:
+
+						for j in ir_exons[ir2]['introns']:
+
+							# Check that the retained introns are not the same and
+							# that they share a common "middle" exon
+							if ir_exons[ir1]['introns'] != ir_exons[ir2]['introns'] and i[1]+1 == int(ir2.split('-')[0]):
+
+								print '{0}\t{1}\t{1};MXI:{0}:{2}:{3}-{4}:{5}-{6}:{7}:{8}\t{9}\t{10}'.format(chromosome, g_id, ir1.split('-')[0], i[0], i[1], j[0], j[1], ir2.split('-')[1], strand, ','.join(ir_exons[ir1]['trs']), ','.join(ir_exons[ir1]['trs']|ir_exons[ir2]['trs']))
 
 if __name__ == '__main__':
 
